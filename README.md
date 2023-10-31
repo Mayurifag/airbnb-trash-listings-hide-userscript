cc939e63295c7d0a97b3113ac441278ae7a6bc9f
Vladislav Ponomarev <farazeus@gmail.com>
2023-11-01 02:54:19 +0700

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Airbnb search enhancer
+
+## Description
+
+This is a userscript that enhances the search experience on Airbnb by hiding
+listings that don't have reviews and high rating.
+
+Default settings (changeable directly in the script src):
+
+* Review count threshold: at least 5
+* Rating threshold: at least 4.75
+
+## Installation
+
+1. Install a userscript manager for your browser, e.g. [ViolentMonkey](https://violentmonkey.github.io/get-it/).
+2. Install the script <https://github.com/Mayurifag/airbnb-trash-listings-hide-userscript/userscript.js>
---
+++ userscript.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         Airbnb trash listings hider
+// @namespace    https://github.com/Mayurifag/airbnb-trash-listings-hide-userscript/userscript.js
+// @version      2023.10.31
+// @description  Hide listings with low rating and low number of reviews on Airbnb search page.
+// @author       Mayurifag
+// @match        https://www.airbnb.com/s/*
+// @grant        none
+// @license MIT
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  const observer = new MutationObserver((mutationsList) => {
+    const ratingPattern = /([\d.]+) \((\d+)\)/;
+    const ratingThreshold = 4.75;
+    const reviewCountThreshold = 5;
+
+    mutationsList.forEach((mutation) => {
+      if (mutation.type === 'childList') {
+        const addedNodes = Array.from(mutation.addedNodes);
+        const itemListElements = addedNodes.filter(node =>
+          node.tagName === 'DIV' && node.getAttribute('itemprop') === 'itemListElement'
+        );
+
+        if (itemListElements.length > 0) {
+          const listing = itemListElements[0];
+          const lines = listing.innerText.split('\n');
+          const listingShortDescription = `${lines[0]} ${lines[1]}`;
+          const lastLine = lines[lines.length - 1];
+          const match = lastLine.match(ratingPattern);
+
+          if (match) {
+            let rating = parseFloat(match[1]);
+            let reviewCount = parseInt(match[2], 10);
+
+            // console.log(`${listingShortDescription} Rating: ${rating}, Review Count: ${reviewCount}`);
+
+            if (rating < ratingThreshold) {
+              console.log(`${listingShortDescription} has low rating`);
+              listing.style.opacity = 0.1;
+              return;
+            }
+
+            if (reviewCount < reviewCountThreshold) {
+              console.log(`${listingShortDescription} has low review count`);
+              listing.style.opacity = 0.2;
+              return;
+            }
+          } else {
+            console.log(`${listingShortDescription} has no rating and reviews`);
+            listing.style.opacity = 0.1;
+          }
+        }
+      }
+    });
+  });
+
+  observer.observe(document, { childList: true, subtree: true });
+})();
