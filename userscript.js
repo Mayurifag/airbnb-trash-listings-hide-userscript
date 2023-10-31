f0eb40c6c9e4b95a4b7230bfa1f97701955a2c8e
Vladislav Ponomarev <farazeus@gmail.com>
2023-11-01 03:52:33 +0700

    Fix/lint via standardjs

--- userscript.js
+++ userscript.js
@@ -10,52 +10,52 @@
 // ==/UserScript==
 
 (function () {
-  'use strict';
+  'use strict'
 
+  /* eslint-env browser */
   const observer = new MutationObserver((mutationsList) => {
-    const ratingPattern = /([\d.]+) \((\d+)\)/;
-    const ratingThreshold = 4.75;
-    const reviewCountThreshold = 5;
+    const ratingPattern = /([\d.]+) \((\d+)\)/
+    const ratingThreshold = 4.75
+    const reviewCountThreshold = 5
 
     mutationsList.forEach((mutation) => {
       if (mutation.type === 'childList') {
-        const addedNodes = Array.from(mutation.addedNodes);
+        const addedNodes = Array.from(mutation.addedNodes)
         const itemListElements = addedNodes.filter(node =>
           node.tagName === 'DIV' && node.getAttribute('itemprop') === 'itemListElement'
-        );
+        )
 
         if (itemListElements.length > 0) {
-          const listing = itemListElements[0];
-          const lines = listing.innerText.split('\n');
-          const listingShortDescription = `${lines[0]} ${lines[1]}`;
-          const lastLine = lines[lines.length - 1];
-          const match = lastLine.match(ratingPattern);
+          const listing = itemListElements[0]
+          const lines = listing.innerText.split('\n')
+          const listingShortDescription = `${lines[0]} ${lines[1]}`
+          const lastLine = lines[lines.length - 1]
+          const match = lastLine.match(ratingPattern)
 
           if (match) {
-            let rating = parseFloat(match[1]);
-            let reviewCount = parseInt(match[2], 10);
+            const rating = parseFloat(match[1])
+            const reviewCount = parseInt(match[2], 10)
 
             // console.log(`${listingShortDescription} Rating: ${rating}, Review Count: ${reviewCount}`);
 
             if (rating < ratingThreshold) {
-              console.log(`${listingShortDescription} has low rating`);
-              listing.style.opacity = 0.1;
-              return;
+              console.log(`${listingShortDescription} has low rating`)
+              listing.style.opacity = 0.1
+              return
             }
 
             if (reviewCount < reviewCountThreshold) {
-              console.log(`${listingShortDescription} has low review count`);
-              listing.style.opacity = 0.2;
-              return;
+              console.log(`${listingShortDescription} has low review count`)
+              listing.style.opacity = 0.2
             }
           } else {
-            console.log(`${listingShortDescription} has no rating and reviews`);
-            listing.style.opacity = 0.1;
+            console.log(`${listingShortDescription} has no rating and reviews`)
+            listing.style.opacity = 0.1
           }
         }
       }
-    });
-  });
+    })
+  })
 
-  observer.observe(document, { childList: true, subtree: true });
-})();
+  observer.observe(document, { childList: true, subtree: true })
+})()
